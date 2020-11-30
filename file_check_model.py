ed557258c114a2a264cd0a073823a9fea4ac8473
snooby <52961872+baeyuna97@users.noreply.github.com>
2020-11-30 15:44:46 +0900

    Add files via upload

--- file_check_model.py
+++ file_check_model.py
@@ -5,10 +5,6 @@ import sys, os, shutil, random, time
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
-# 변경사항 : 1. 구분 기준 (날짜 -> 파일 확장자별?) & 생성 날자별 (기존 파일명에 날짜를 넣어 랜덤하게 생성 -> 파일 정보상의 생성 날짜를 가져와 자동 분류해줌)
-#          2. 폴더 path 입력 받기
-#          3. 분류된 파일, 군집 별로 폴더 생성 후 폴더 안에 저장
-
 # What To Do : 1. 결과 표로 보이게 생성
 
 
@@ -16,15 +12,27 @@ class main(QWidget):
     def __init__(self):
         super().__init__()
         # self.srcDir = "/Users/baeyuna/Documents/univ/swp2/12-1"
-        self.extensions = [".py", ".ipynb", ".pdf", 'zip', 'dat', 'csv']
+        self.extensions = ["py", "ipynb", "pdf", 'zip', 'dat', 'csv']
+        #  분류 폴더 경로 지정
+        self.pathlabel = QLabel("분류할 폴더 경로 지정 => ")
         self.pathedit = QLineEdit()
-        self.pathlbl = QLabel(self)
-        self.pathedit.setText("                                                                 분류할 폴더 경로를 입력하세요. ")
-        self.searchbutton = QPushButton("경로지정")
-        self.btnClss = QPushButton("확장자 파일분류")
-        self.btnClss2 = QPushButton("생성날짜 파일분류")
-        self.btnmove = QPushButton("분류 폴더생성")
-        self.resultlbl = QLabel(self)
+        #  정리될 폴더 경로 지정
+        self.folderlabel = QLabel("정리될 폴더 경로 지정 => ")
+        self.folderedit = QLineEdit()
+        # 분류 방법
+        self.cblabel = QLabel("분류 방법 : ")
+        self.cbedit = QLineEdit()
+        self.cbedit.setText("            키워드 분류시 분류할 키워드를 입력하세요. (띄어쓰기 기준)")
+        self.cb = QComboBox()
+        self.cb.addItem('확장자별 분류')
+        self.cb.addItem('생성날짜별 분류')
+        self.cb.addItem('키워드별 분류')
+        # OK 버튼
+        self.okbutton = QPushButton("OK")
+        # 결과창
+        self.resultlbl1 = QLabel(self)
+        self.resultlbl2 = QLabel(self)
+        self.resultlbl3 = QLabel(self)
         self.setUi()
         self.setSlot()
 
@@ -32,95 +40,130 @@ class main(QWidget):
         self.setGeometry(300, 300, 700, 400)
         self.setWindowTitle("파일 식별기")
 
-        # self.tv.setModel(self.model)
-        # self.table.setColumnCount(len(self.extensions))
-        # self.table.setRowCount(1)
+        # 폴더 위젯
+        hbox1 = QHBoxLayout()
+        hbox1.addWidget(self.pathlabel)
+        hbox1.addWidget(self.pathedit)
+
+        # 정리 폴더 위젯
+        hbox2 = QHBoxLayout()
+        hbox2.addWidget(self.folderlabel)
+        hbox2.addWidget(self.folderedit)
+
+        # 분류 박스 위젯
+        hbox3 = QHBoxLayout()
+        hbox3.addWidget(self.cbedit)
+        hbox3.addWidget(self.cblabel)
+        hbox3.addWidget(self.cb)
+        hbox3.stretch(1)
+
+        # OK 버튼 위젯
+        hbox4 = QHBoxLayout()
+        hbox4.addWidget(self.okbutton)
+        hbox4.stretch(1)
+
         vbox1 = QVBoxLayout()
-        vbox1.addWidget(self.pathedit)
-        vbox1.addWidget(self.searchbutton)
-
-        hbox = QHBoxLayout()
-        hbox.addWidget(self.btnClss)
-        hbox.addWidget(self.btnClss2)
-
-        vbox2 = QVBoxLayout()
-        vbox2.addWidget(self.btnmove)
-        vbox2.addWidget(self.pathlbl)
-        vbox2.addWidget(self.resultlbl)
+        vbox1.addWidget(self.resultlbl1)
+        vbox1.addWidget(self.resultlbl2)
+        vbox1.addWidget(self.resultlbl3)
 
         vbox = QVBoxLayout()
+        vbox.addLayout(hbox1)
+        vbox.addLayout(hbox2)
+        vbox.addLayout(hbox3)
+        vbox.addLayout(hbox4)
         vbox.addLayout(vbox1)
-        vbox.addLayout(hbox)
-        vbox.addLayout(vbox2)
         self.setLayout(vbox)
 
-    def pathClicked(self):
-        self.file_path = self.pathedit.text()
-        self.pathedit.clear()
-        self.file_list = os.listdir(self.file_path)
-        self.pathlbl.setText(f"{self.file_path} 폴더 파일을 분류하겠습니다.")
-        return
-
+    # 위젯 동작 연결
     def setSlot(self):
-        self.searchbutton.clicked.connect(self.pathClicked)
-        self.btnClss.clicked.connect(self.classify)
-        self.btnClss2.clicked.connect(self.check_publishtime)
-        self.btnmove.clicked.connect(self.folder_move)
+        self.okbutton.clicked.connect(self.okClicked)
         return
 
-    def classify(self):
-        self.file_count_dict = dict(zip(self.extensions, [0]*len(self.extensions)))
+    # 확장자별 분류 함수
+    def ex_classify(self):
+        self.file_count_dict = {}
         for extension in self.extensions:
-            file_num = len([file for file in self.file_list if file.endswith(extension)])
-            self.file_count_dict[extension] = file_num
+            for file in self.file_list:
+                if file.endswith(extension):
+                    if extension not in self.file_count_dict:
+                        self.file_count_dict[extension] = [file]
+                    else:
+                        self.file_count_dict[extension].append(file)
 
         answer = ""
         for i in range(len(self.file_count_dict)):
-            answer += f"{list(self.file_count_dict.values())[i]}개의 {list(self.file_count_dict.keys())[i]}가 존재합니다. \n"
+            answer += f"{list(self.file_count_dict.keys())[i]} extension file : {len(list(self.file_count_dict.values())[i])}개 \n"
 
-        self.resultlbl.setText(answer)
+        self.resultlbl2.setText(answer)
         return
 
-    def check_publishtime(self):
+    # 생성날짜별 분류 함수
+    def time_classify(self):
         self.file_time_dict = {}
         for file in self.file_list:
             file_time = time.ctime(os.path.getctime(f"{self.file_path}/{file}"))
             times = file_time.split(" ")
             year = times[4]; month = times[1]; day = times[2];
-            str_time = f"{year} {month} {day}"
+            str_time = f"{year}_{month}_{day}"
             if str_time not in self.file_time_dict:
-                self.file_time_dict[str_time] = 1
+                self.file_time_dict[str_time] = [file]
             else:
-                self.file_time_dict[str_time] += 1
+                self.file_time_dict[str_time].append(file)
         self.file_time_dict = sorted(self.file_time_dict.items(), reverse=True)
 
         answer = ""
         for i in range(len(self.file_time_dict)):
-            answer += f"{self.file_time_dict[i][0]}에 생성한 파일이 {self.file_time_dict[i][1]}개 존재합니다. \n"
+            answer += f"{self.file_time_dict[i][0]} date created file : {len(self.file_time_dict[i][1])}개 \n"
 
-        self.resultlbl.setText(answer)
+        self.resultlbl2.setText(answer)
         return
 
-    # 폴더 생성 및 분류
+    # 키워드별 분류 함수
+    def keyword_classify(self):
+        keyword_text = self.cbedit.text()
+        keywords = keyword_text.split(" ")
+        self.keyword_dict = {}
+        for keyword in keywords:
+            for file in self.file_list:
+                if keyword in file:
+                    if keyword not in self.keyword_dict:
+                        self.keyword_dict[keyword] = [file]
+                    else:
+                        self.keyword_dict[keyword].append(file)
+
+        answer = ""
+        for i in range(len(self.keyword_dict)):
+            answer += f"{list(self.keyword_dict.keys())[i]} keyword file : {len(list(self.keyword_dict.values())[i])}개 \n"
+
+        self.resultlbl2.setText(answer)
+        return
+
+    # 폴더 분류 함수
     def folder_move(self):
+        self.criterion = self.cb.currentText()
+        self.folderpath = self.folderedit.text()
+        self.folderedit.clear()  # 다음 연속해서 값을 받기위해 위젯 초기화
+
         # 확장자별 분류
-        if len(self.resultlbl.text().split(" ")) == 3:
+        if self.criterion == "확장자별 분류":
             for extenstion in self.extensions:
+                # 분류 폴더 생성
                 try:
-                    dir = self.file_path + "/" + extenstion
+                    dir = self.folderpath + "/" + extenstion
                     os.makedirs(dir)
                     for file in self.file_list:
                         if file.endswith(extenstion):
-                            shutil.move(self.file_path+"/"+file, dir+"/"+file)
+                            shutil.copy(file, self.folderpath+"/"+file)
                 except:
                     print(f"Error: Creating directory. {dir}")
 
         # 생성 날짜별 분류
-        else:
+        elif self.criterion== "생성날짜별 분류":
             for key in self.file_time_dict:
                 date = key[0]
                 try:
-                    dir = self.file_path + "/" + date
+                    dir = self.folderpath + "/" + date
                     os.makedirs(dir)
                     for file in self.file_list:
                         file_time = time.ctime(os.path.getctime(f"{self.file_path}/{file}"))
@@ -130,11 +173,49 @@ class main(QWidget):
                         day = times[2];
                         str_time = f"{year} {month} {day}"
                         if str_time == date:
-                            shutil.move(self.file_path+"/"+file. dir+"/"+file)
+                            shutil.copy(file, self.folderpath+"/"+file)
                 except:
                     print(f"Error: Creating directory. {dir}")
 
+        # 키워드별 분류
+        elif self.criterion == "키워드별 분류":
+            for i in range(len(self.keyword_dict)):
+                try:
+                    dir = self.folderpath + "/" + list(self.keyword_dict.keys())[i]
+                    os.makedirs(dir)
+                    files = self.keyword_dict.values()[i]
+                    for file in files:
+                        shutil.copy(file, self.folderpath+"/"+file) # self.file_path+"/"+
+                except:
+                    print(f"Error: Creating directory. {dir}")
 
+        return
+
+    def okClicked(self):
+        # 1. 경로 폴더 가져오기.
+        self.file_path = self.pathedit.text() # 지정된 폴더 경로 가져오기.
+        self.pathedit.clear() # 다음 연속해서 값을 받기위해 위젯 초기화
+        self.file_list = os.listdir(self.file_path) # 해당 지정 경로의 파일 리스트 가져오기.
+        self.resultlbl1.setText(f"{self.file_path} 폴더 파일을 분류하겠습니다.")  #  분류할 폴더 위치 사용자 보여주기
+
+        # 2. 폴더 지정 분류 기준으로 분류하기
+        self.criterion = self.cb.currentText() # 분류기준 가져오기.
+        if self.criterion == "확장자별 분류":
+            self.ex_classify()
+
+        elif self.criterion== "생성날짜별 분류":
+            self.time_classify()
+
+        elif self.criterion == "키워드별 분류":
+            self.keyword_classify()
+
+        # 3. 분류된 폴더 지정된 폴더로 분류하기
+        self.folder_move()
+        self.resultlbl3.setText(
+            f"{self.file_path} 폴더의 파일 {len(self.file_list)}개를 {self.criterion}에 따라 \n \t 지정된 폴더 {self.folderpath}에 분류 완료하였습니다.")
+        # 4. Table view showing
+
+        return
 
 if __name__ == '__main__':
     app = QApplication([])
